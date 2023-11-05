bc396aba89b6d0d82d28c7a65f785ea7ca0755bc
Tiago França <tiagofranca.rita@gmail.com>
2023-11-05 19:43:37 -0300

    commit initial

---
+++ src/main/java/br/com/franca/kfka/KfkaApplication.java
@@ -0,0 +1,13 @@
+package br.com.franca.kfka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfkaApplication.class, args);
+	}
+
+}
